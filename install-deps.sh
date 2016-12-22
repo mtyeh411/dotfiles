567f019d14eda0235833591bc5234d5df953eb75
Matt Yeh <dev.mtyeh411@gmail.com>
2016-12-21 22:36:53 -0500

    use git-friendly

--- install-deps.sh
+++ install-deps.sh
@@ -2,10 +2,10 @@
 curl -L https://get.rvm.io | bash -s stable --rails --autolibs=enable
 
 # pow
-curl get.pow.cx | sh
+#curl get.pow.cx | sh
 
 # homebrew
 ruby -e "$(curl -fsSkL https://raw.github.com/mxcl/homebrew/go)"
 
 # git-friendly
-# bash < <( curl https://raw.github.com/jamiew/git-friendly/master/install.sh)
+bash < <( curl https://raw.github.com/jamiew/git-friendly/master/install.sh)
