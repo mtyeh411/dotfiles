ee412975fd91bda51fc0fb080e084f60458c44cc
Matt Yeh <dev.mtyeh411@gmail.com>
2020-12-06 13:07:29 -0500

    streamline deps

--- install-deps.sh
+++ install-deps.sh
@@ -1,11 +1,2 @@
-﻿# rvm
-curl -L https://get.rvm.io | bash -s stable --rails --autolibs=enable
-
-# pow
-#curl get.pow.cx | sh
-
 # homebrew
 ruby -e "$(curl -fsSkL https://raw.github.com/mxcl/homebrew/go)"
-
-# git-friendly
-bash < <( curl https://raw.github.com/jamiew/git-friendly/master/install.sh)
